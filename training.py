bd64a1aefa6a6203566a05acca151293d5fb373a
Catfish New Laptop <pettyspenceralexander@gmail.com>
2023-06-27 13:18:32 -0500

    tqdm improvements

--- training.py
+++ training.py
@@ -1,5 +1,5 @@
 import torch
-from tqdm import tqdm
+from tqdm.notebook import tqdm
 
 # Doing in-place allows easier saving via pickle
 def train_vae(model, dataloader, nepochs=100, inter_epoch  = True, regularize = False):
@@ -10,10 +10,11 @@ def train_vae(model, dataloader, nepochs=100, inter_epoch  = True, regularize = 
     else:
         optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     try:
-        progress = tqdm(range(nepochs))
+        progress = tqdm(range(nepochs), position = 0, unit = "epoch")
         for epoch in progress: # Has a counter that increments each epoch
             train_loss = 0
-            for batch_idx, (data, _) in enumerate(dataloader):
+            batches = tqdm(dataloader, position = 1, unit = "batch", leave = False)
+            for batch_idx, (data, _) in enumerate(batches):
                 optimizer.zero_grad()
                 x_hat = model(data)
                 term1 = ((data - x_hat)**2).mean()
@@ -22,8 +23,10 @@ def train_vae(model, dataloader, nepochs=100, inter_epoch  = True, regularize = 
                 loss.backward()
                 train_loss += loss.item()
                 optimizer.step()
+                batches.set_description(f"Batch [{batch_idx+1}/{len(dataloader)}]")
+                batches.set_postfix(Loss = loss.item())
             progress.set_description(f"Epoch [{epoch+1}/{nepochs}]")
             progress.set_postfix(Loss = loss.item())
     except KeyboardInterrupt:
         print('Exited from training early')
-            
+        
