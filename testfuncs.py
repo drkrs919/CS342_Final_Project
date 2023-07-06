c3faa755b5d4931c93c7a5adc4862286a1214d0f
SpencerPettyDesktop <spencerpetty@utexas.edu>
2023-07-06 18:56:40 -0500

    refactor test funcs into one test_model func
    Former-commit-id: aab7b2f81caae8235e8b00bf0151b5f4b853e325

--- testfuncs.py
+++ testfuncs.py
@@ -3,29 +3,13 @@ import cv2
 import matplotlib.pyplot as plt
 import torch
 import numpy as np
-import pickle
 from processpics import crop_size
 
 
-# Test the syntax and dimensionality of the model on one sample. Expect noise as output
-def test_noise_generation(nodes = 64, gray = False):
-    model = VAE(10, nodes)
-    i = np.random.choice(13234)
-    image_path = f'./lfwdatacolor/Faces/image_{i}.jpg'
-    if gray:
-        image_path = f'./lfwdatagray/Faces/image_{i}.jpg'
-    imgnp = cv2.imread(image_path)
-    if imgnp is None:
-        test_noise_generation(nodes = nodes, gray = gray)
-    else:
-        imgnp = cv2.cvtColor(imgnp, cv2.COLOR_BGR2RGB)
-        img = torch.reshape(torch.tensor(imgnp, dtype = torch.float32), (1, -1, crop_size[0], crop_size[1]))
-        out = model(img)
-        plt.imshow(imgnp)
-        plt.matshow(out.squeeze().detach().reshape((crop_size[0], crop_size[1], -1)))
-
-
-def compare_outputs(model, pretrain = True, gray = False):
+def test_model(model = None, pretrain = True, gray = False):
+    usemodel = model
+    if usemodel == None:
+        usemodel = VAE(5, 32)
     image_path = None
     if pretrain:
         i = np.random.choice(13234)
@@ -39,15 +23,15 @@ def compare_outputs(model, pretrain = True, gray = False):
             image_path = f'./profdatagray/Faces/image_{i}.jpg'
     imgnp = cv2.imread(image_path)
     if imgnp is None:
-        compare_outputs(model, pretrain = pretrain, gray = gray)
+        test_model(usemodel, pretrain = pretrain, gray = gray)
     else:
         imgnp = cv2.cvtColor(imgnp, cv2.COLOR_BGR2RGB)
         img = torch.reshape(torch.tensor(imgnp, dtype = torch.float32), (1, -1, crop_size[0], crop_size[1]))
-        out = model(img)
+        out = usemodel(img)
         plt.imshow(imgnp)
         plt.matshow(out.squeeze().detach().reshape((crop_size[0], crop_size[1], -1)))
 
 
 def compare_and_gen(model):
-    compare_outputs(model, pretrain = False)
+    test_model(model = model, pretrain = False)
     model.generate_face()
